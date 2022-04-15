45f994196c17a41dc750b6c6eebdc730680c730a
AgamCS <cherraagam@gmail.com>
2022-04-15 12:55:40 -0700

    changed unstable color description

--- lua/revivalservers_covenantshield/sh_config.lua
+++ lua/revivalservers_covenantshield/sh_config.lua
@@ -13,7 +13,7 @@ end
 revivalservers_covShield.config.health = 400 // How much damage the shield asborbs before deactivating
 revivalservers_covShield.config.pickupTime = 5 // How long, in seconds, it takes to pickup the shield
 revivalservers_covShield.config.shieldRadius = 150 // Raidus the owner needs to stay to keep the shield active
-revivalservers_covShield.config.unstableColor = Color(255, 180, 212) // Color to set the entity to when it goes unstable. Set to nil to disable changing colors
+revivalservers_covShield.config.unstableColor = Color(255, 180, 212) // Color to set the entity the closer it reaches unstable. Set to nil to disable changing color
 revivalservers_covShield.config.cancelActionVel = 50 // The velocity the player needs to reach to cancel picking up the shield
 
 revivalservers_covShield.config.throwVelocity = 10 // The velocity at which the player throws the shield entity
