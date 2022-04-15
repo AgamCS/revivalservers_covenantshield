89a0d027335e7a2cc206a5b470cffc6bdf6c11a8
AgamCS <cherraagam@gmail.com>
2022-04-15 01:42:17 -0700

    fixed typo

--- lua/entities/cov_shield/init.lua
+++ lua/entities/cov_shield/init.lua
@@ -3,20 +3,19 @@ include("shared.lua")
 AddCSLuaFile("shared.lua")
 AddCSLuaFile("cl_init.lua")
 
-local cfg = revivalservers_covSheild.cfg
-
 function ENT:Initialize()
     self:SetModel("models/hawksshield/hawksshield.mdl")
     self:PhysicsInit(SOLID_VPHYSICS)
     self:SetMoveType(MOVETYPE_VPHYSICS)
     self:SetSolid(SOLID_VPHYSICS)
     self:SetUseType(SIMPLE_USE)
-    self.raidus = cfg.shieldRadius * cfg.shieldRadius
-    self.unstableColor = cfg.unstableColor
+    --self.raidus = revivalservers_covShield.config.shieldRadius * revivalservers_covShield.config.shieldRadius
+    --self.unstableColor = revivalservers_covShield.config.unstableColor
     self.state = "stable"
-
+    --print(self.radius)
     local phys = self:GetPhysicsObject()
-    print(phys:IsValid())
+    print("phys" .. tostring(phys:IsValid()))
+    PrintTable(revivalservers_covShield)
     if phys:IsValid() then
         phys:Wake()
     end
@@ -24,10 +23,12 @@ function ENT:Initialize()
 end
 
 function ENT:Use(activator, caller)
+    
     local owner = self:GetOwner()
     if activator != owner then return end 
+    if activator:KeyDown(IN_WALK) then return end
     self:SetisGettingPickedup(true)
-    revivalservers_covSheild.UseShield(self, owner)
+    revivalservers_covShield.UseShield(self, owner)
 end
 
 function ENT:OnTakeDamage( dmginfo )
@@ -42,12 +43,13 @@ function ENT:OnTakeDamage( dmginfo )
 end
 
 function ENT:SetupDataTables()
-    self:NetworkVar( "Int", cfg.pickupTime, "PickupTime" )
+    self:NetworkVar( "Int", revivalservers_covShield.config.pickupTime, "PickupTime" )
     self:NetworkVar("Bool", false, "isGettingPickedup")
 end
 
 function ENT:Think()
-    if self:GetOwner():GetPos():DistToSqr(self:GetPos()) > self.raidus then
+    --print( self:GetOwner():GetPos():DistToSqr(self:GetPos()) )
+    --[[if self:GetOwner():GetPos():DistToSqr(self:GetPos()) > self.raidus then
         SafeRemoveEntity(self)
-    end
+    end--]]
 end
