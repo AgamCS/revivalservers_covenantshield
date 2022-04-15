a182b67333caaaf8aad84cd1e53b2c48efbd14f4
AgamCS <cherraagam@gmail.com>
2022-04-15 16:35:44 -0700

    fixed hud

--- lua/revivalservers_covenantshield/cl_actions.lua
+++ lua/revivalservers_covenantshield/cl_actions.lua
@@ -1,15 +1,21 @@
+local cfg = revivalservers_covShield.config
 net.Receive("cov_shield_action", function()
     local ent = net.ReadEntity()
-    timer.Create(ply:SteamID64() .. " cov_shieldPickup", ent:GetPickupTime(), 1, function() hook.Remove("HUDPaint", "coventantShield_pickupTimer") timer.Remove(ply:SteamID64() .. " cov_shieldPickup") end)
+    timer.Create(ply:SteamID64() .. " cov_shieldPickup", cfg.pickupTime, 1, function() hook.Remove("HUDPaint", "coventantShield_pickupTimer") timer.Remove(ply:SteamID64() .. " cov_shieldPickup") end)
+    local startTime = CurTime()
     hook.Add("HUDPaint", "coventantShield_pickupTimer", function()
-        local timeleft = math.Round(timer.TimeLeft((ply:SteamID64() .. " cov_shieldPickup"), 1)
-        local endtime = delay - timeleft
+        local time = startTime + cfg.pickupTime - startTime
+        local curtime = CurTime() - startTime
+        local timeleft = math.Round( timer.TimeLeft( (ply:SteamID64() .. " cov_shieldPickup"), 1) )
+        local endtime = cfg.pickupTime - timeleft
         local scrw, scrh = ScrW(), ScrH()
-        local x, y, width, height = scrw / 2 - scrw / 20, scrh / 2 - 60, scrw / 10, scrh / 15
-        local barWidth = Lerp( (FrameTime() * (width * 0.64 * endtime)) / 2, width * 0.05, width * 0.9)
-        draw.RoundedBox(0, x, y, width, height, )
-        draw.RoundedBox(0, x + 8, y + 7, barWidth, height / 1.2, )
-        draw.SimpleText(timeleft, "covShield_font", scrw / 2 , scrh / 2 - 25, color_white, 1, 1)
+        local x, y, width, height = scrw * 0.5 - scrw * 0.15, scrh * 0.8 - scrh * 0.05, scrw * 0.3, scrh * 0.07
+        local status = math.Clamp(curtime / time, 0, 1)
+        local barWidth = status * width - 16
+        draw.RoundedBox(0, x, y, width, height, cfg.backgroundBarColor)
+        draw.RoundedBox(0, x + 8, y + 7, barWidth, height * 0.2, cfg.foregroundBarColor )
+        draw.SimpleText("PICKING UP SHIELD", "covShield_font", scrw * 0.5 , scrh * 0.8 - 20, color_white, 1, 1)
+        draw.SimpleText(timeleft, "covShield_font", scrw * 0.5 , scrh * 0.8, color_white, 1, 1)
     end)
 end)
 
